ce900b0f7300a29a3d5bafe7e4e7a07012549760
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-10-10 12:57:15 -0500

    wip

--- src/main/java/com/circulosiete/spring/sample/war/controller/IndexController.java
+++ src/main/java/com/circulosiete/spring/sample/war/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.circulosiete.spring.sample.war.controller;
 
+import com.circulosiete.spring.sample.war.service.RandomService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-  @RequestMapping("/mvc")
+  private final RandomService randomService;
+
+  public IndexController(RandomService randomService) {
+    this.randomService = randomService;
+  }
+
+  @RequestMapping("/")
   public String showHomePage(Model m) {
-    m.addAttribute("name", "Este es un valor establecido en un controller de Spring MVC");
+    m.addAttribute("name", randomService.saludo() + ", este es un valor establecido en un controller de Spring MVC");
     return "index";
   }
 }
---
+++ src/main/java/com/circulosiete/spring/sample/war/service/RandomService.java
@@ -0,0 +1,10 @@
+package com.circulosiete.spring.sample.war.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RandomService {
+  public String saludo() {
+    return "Hola!";
+  }
+}
